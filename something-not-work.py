57cd477a47399e9d9cc3f705e35575a64573cccc
Oleh_Yezerskyi <lololeg2929@gmail.com>
2025-11-21 12:59:04 +0100

    Revert "Update something-not-work.py"
    This reverts commit 804c32e5f464a09dd9a04915d7fa998a934a2471.

--- something-not-work.py
+++ something-not-work.py
@@ -1,5 +1,5 @@
 name = input("What is your name?")
 if True ("Wandy") :
-      print("Hello, Wendy") #commentar
+      print("Hello, Wendy")
 if False ("Wandy"):
       print("Sorry, I don’t know you.")
